0b18a48dab08d05271cdc5ce4472c99c5997072b
Nick Levkovich <nicklevkovich@gmail.com>
2018-10-05 14:46:39 +0300

    52/102 passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+    let set = new Set(array),
+        length = 0,
+        arrL = set.size;
+
+    if (arrL === 0) return (arrL);
+    for (let i = 0; i < arrL; i++) {
+        let left = i - 1,
+            right = i + 1,
+            count = 1;
+
+        while (set.has(left)) {
+            count++;
+            set.delete(left);
+            left--;
+        };
+        while (set.has(right)) {
+            count++;
+            set.delete(right);
+            right++;
+        };
+        length = Math.max(count, length);
+    };
+
+    return (length);
+};
