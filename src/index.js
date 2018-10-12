0dffda55d61226e53d07898c783ae6c4e8a67848
Nick Levkovich <nicklevkovich@gmail.com>
2018-10-12 04:06:49 +0300

    Refactored Final Solution. 102/102 passing.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 module.exports = function longestConsecutiveLength(array) {
-    let set = new Set(array),
-        length = 0,
-        arrL = set.size;
+  let set = new Set(array),
+      longestLength = 0,
+      arrayLength = array.length;
 
-    if (arrL === 0) return 0;
-    for (let i = 0; i < arrL; i++) {
-        let left = array[i] - 1,
-            right = array[i] + 1,
-            count = 1;
+  if (arrayLength === 0) return 0;
+  for (let i = 0; i < arrayLength; i++) {
+      let left = array[i] - 1,
+          right = array[i] + 1,
+          count = 1;
 
-        while (set.has(left)) {
-            count++;
-            set.delete(left);
-            left--;
-        };
-        while (set.has(right)) {
-            count++;
-            set.delete(right);
-            right++;
-        };
-        length = Math.max(length, count);
-    };
-    return (length);
+      while (set.has(left)) {
+          count++;
+          set.delete(left);
+          left--;
+      };
+      while (set.has(right)) {
+          count++;
+          set.delete(right);
+          right++;
+      };
+      longestLength = Math.max(longestLength, count);
+  };
+  return (longestLength);
 };
